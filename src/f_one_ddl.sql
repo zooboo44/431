49a6f7c3fcdb098be538084a5e9f174998536d6c
Aaron <shyguydude01298@gmail.com>
2026-04-16 15:56:31 -0700

    Created basic database creation

--- src/f_one_ddl.sql
+++ src/f_one_ddl.sql
@@ -1,1 +1,7 @@
--- TODO: CREATE DDL IN CLASS
+-- Always rebuild the database entirely when running this file
+DROP DATABASE IF EXISTS FORMULA_ONE;
+CREATE DATABASE IF NOT EXISTS FORMULA_ONE;
+
+-- Use the new database we just made
+USE FORMULA_ONE;
+
