59fc2da41f69d45be46bb7c17c83d9d914b1561c
Aaron <shyguydude01298@gmail.com>
2026-04-16 20:10:45 -0700

    Created circuits table

--- src/f_one_ddl.sql
+++ src/f_one_ddl.sql
@@ -6,7 +6,7 @@ USE FORMULA_ONE;
 -- TABLES
 -------------------------------------------------------------------------------
 
-CREATE TABLE driver(
+CREATE TABLE drivers(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50)NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE driver(
 );
 
 -- Teams have exactly two drivers
-CREATE TABLE team(
+CREATE TABLE teams(
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     team_name VARCHAR(255) NOT NULL,
     driver_a_id INT UNSIGNED NOT NULL,
@@ -34,6 +34,19 @@ CREATE TABLE team(
 
     FOREIGN KEY (driver_a_id) REFERENCES driver(id) ON DELETE CASCADE,
     FOREIGN KEY (driver_b_id) REFERENCES driver(id) ON DELETE CASCADE
+);
+
+CREATE TABLE circuits(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    length_km FLOAT NOT NULL,
+    laps INT NOT NULL,
+    lap_record_ms INT UNSIGNED,
+    lap_record_holder INT UNSIGNED
+    last_modified TIMESTAMP CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (lap_record_holder) REFERENCES driver(id) ON DELETE CASCADE
 );
 
 -------------------------------------------------------------------------------
