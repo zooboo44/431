dacb3ee6c6ca6f857aaf643502407bcd33564aef
Aaron <shyguydude01298@gmail.com>
2026-04-22 12:53:49 -0700

    Function bank now requires all functions from the functions directory

--- src/function_bank.php
+++ src/function_bank.php
@@ -1,1 +1,17 @@
-<?php echo "Unfinished page!" ?>
+<?php
+
+// Directory setup
+$base_path = $_SERVER['DOCUMENT_ROOT'].'/final_project';
+$functions_directory = $base_path.'/functions';
+
+// Find all files ending in .php
+$files = glob($functions_directory."*php");
+
+// Requires every file inside of the functions folder to load them all in with one require_once call
+if ($files) {
+    foreach ($files as $file) {
+        require_once($file);
+    }
+}
+
+?>
